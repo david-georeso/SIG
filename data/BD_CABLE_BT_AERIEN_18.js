d5d7dc8bf28dee899536621f1982bf5f6bb53d85
david-georeso <58975016+david-georeso@users.noreply.github.com>
2019-12-18 16:50:56 +0100

    Add files via upload

---
+++ data/BD_CABLE_BT_AERIEN_18.js
@@ -0,0 +1,8 @@
+var json_BD_CABLE_BT_AERIEN_18 = {
+"type": "FeatureCollection",
+"name": "BD_CABLE_BT_AERIEN_18",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "Layer": "BT AERIEN", "PaperSpace": null, "SubClasses": "AcDbEntity:AcDbPolyline", "Linetype": null, "EntityHand": "1569", "Text": null }, "geometry": { "type": "MultiLineString", "coordinates": [ [ [ 9.445674417245742, 42.56320662587558 ], [ 9.448050307614059, 42.563115535359572 ] ] ] } }
+]
+}
---
+++ data/BD_CABLE_HTA_16.js
@@ -0,0 +1,11 @@
+var json_BD_CABLE_HTA_16 = {
+"type": "FeatureCollection",
+"name": "BD_CABLE_HTA_16",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "Layer": "HTA SOUTERRAIN", "PaperSpace": null, "SubClasses": "AcDbEntity:AcDbPolyline", "Linetype": null, "EntityHand": "1542", "Text": null }, "geometry": { "type": "MultiLineString", "coordinates": [ [ [ 9.440920413184431, 42.566640382008593 ], [ 9.441036796922598, 42.56488797791058 ] ] ] } },
+{ "type": "Feature", "properties": { "Layer": "HTA SOUTERRAIN", "PaperSpace": null, "SubClasses": "AcDbEntity:AcDbPolyline", "Linetype": null, "EntityHand": "1543", "Text": null }, "geometry": { "type": "MultiLineString", "coordinates": [ [ [ 9.441036796922598, 42.56488797791058 ], [ 9.441151760146946, 42.56355518060839 ] ] ] } },
+{ "type": "Feature", "properties": { "Layer": "HTA SOUTERRAIN", "PaperSpace": null, "SubClasses": "AcDbEntity:AcDbPolyline", "Linetype": null, "EntityHand": "1544", "Text": null }, "geometry": { "type": "MultiLineString", "coordinates": [ [ [ 9.441151760146946, 42.56355518060839 ], [ 9.441228802664629, 42.562895187645971 ] ] ] } },
+{ "type": "Feature", "properties": { "Layer": null, "PaperSpace": null, "SubClasses": null, "Linetype": null, "EntityHand": null, "Text": null }, "geometry": { "type": "MultiLineString", "coordinates": [ ] } }
+]
+}
