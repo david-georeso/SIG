1c7d5f5c2f8a8794e45e0392b9d46cfc9828a370
david-georeso <58975016+david-georeso@users.noreply.github.com>
2019-12-19 17:03:03 +0100

    Add files via upload

---
+++ data/ADRESSEDESMAIRIES_1.js
@@ -0,0 +1,9 @@
+var json_ADRESSEDESMAIRIES_1 = {
+"type": "FeatureCollection",
+"name": "ADRESSEDESMAIRIES_1",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "INSTIUTION": "MAIRIE", "MAIRIE": "BORGO", "SIG ECLAIRAGE PUBLIQUE": "https:\/\/david-georeso.github.io\/sig\/", "X": 1229324.8, "Y": 6184515.6 }, "geometry": { "type": "Point", "coordinates": [ 9.444648874456117, 42.565872586448037 ] } },
+{ "type": "Feature", "properties": { "INSTIUTION": "GENDARMERIE", "MAIRIE": "BIGUGLIA", "SIG ECLAIRAGE PUBLIQUE": null, "X": 1227933.2, "Y": 6190116.8 }, "geometry": { "type": "Point", "coordinates": [ 9.433324605423195, 42.617079504140364 ] } }
+]
+}
